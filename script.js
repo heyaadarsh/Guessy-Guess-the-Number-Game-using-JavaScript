f2f878003504caab72ea4c414da626039649e4b6
heyaadarsh <aadarsh0001.be21@chitkara.edu.in>
2024-04-26 17:12:06 +0530

    Guessy: Guess the number game using JavaScript

---
+++ script.js
@@ -0,0 +1,103 @@
+let num = parseInt(Math.random()*100+1);
+
+const guessyTitle = document.getElementById('topLine');
+let input = document.getElementById('inp');
+const submit = document.getElementById('sbt');
+const lowOrHi = document.querySelector('.lowOrHi');
+const prev = document.querySelector('.prevGuess');
+const rem = document.querySelector('.remGuess');
+const startOver = document.querySelector('.results')
+
+// const p = document.createElement('p');
+let prevGuess = [];
+let guessRem = 1;
+let playGame = true;
+
+if(playGame){
+    submit.addEventListener('click', (e)=>{
+        e.preventDefault();
+        const userVal = parseInt(input.value);
+        // console.log(userVal);
+        validateGuess(userVal);
+    })
+}
+
+function validateGuess(guess){
+    if(isNaN(guess)){
+        guessyTitle.innerHTML = `Uh Oh! Please Enter a Number.`;
+    }
+    else if(guess<1){
+        guessyTitle.innerHTML = `Your number is less than 1. Enter a number between 1 to 100.`;
+    }
+    else if(guess>100){
+        guessyTitle.innerHTML = `Your number is greater than 100. Enter a number between 1 to 100.`;
+    }
+    else{
+        prevGuess.push(guess);
+        if(guessRem===10){
+            displayGuess(guess);
+            displayMessage(`OOPS! The number was ${num}. Play Again!!`)
+            endGame();
+        }
+        else{
+            displayGuess(guess);
+            checkGuess(guess);
+        }
+    }
+
+}
+
+function checkGuess(guess){
+    if(guess===num){
+        guessyTitle.innerHTML = `WoHoo! You guessed it right, it was ${num}.`;
+        endGame();
+    }
+    else if(guess<num){
+        guessyTitle.innerHTML = `You Guess is too small.`;
+    }
+    else{
+        guessyTitle.innerHTML = `You Guess is too large.`;
+    }
+
+}
+
+function displayGuess(guess){
+    input.value = '';
+    prev.innerHTML += `${guess}, `;
+    guessRem++;
+    rem.innerHTML = `Remaining Guess: ${11 - guessRem}`;
+}
+
+function displayMessage(message){
+    guessyTitle.innerHTML = `${message}`;
+
+}
+
+function endGame(){
+    input.val = '';
+    input.setAttribute('disabled', '');
+    // p.classList.add('button');
+    // p.innerHTML = `<p id="restart">Click here to restart the game!!</p>`;
+
+    lowOrHi.innerHTML = `<p id="restart">Click here to restart the game!!</p>`;
+    // lowOrHi.appendChild(p);
+    playGame = false;
+    newGame();
+}
+
+function newGame(){
+    const newGameButton = document.querySelector('#restart');
+    newGameButton.addEventListener('click', (e)=>{
+        prevGuess = [];
+        guessRem = 1;
+        prev.innerHTML = `Previous Guesses: `;
+        rem.innerHTML = `${11 - guessRem}`;
+        input.removeAttribute('disabled');
+        // startOver.removeChild(p);
+        lowOrHi.innerHTML = `Let's play the game!!`;
+        playGame = true;
+        guessyTitle.innerHTML = `So Again, I am thinking of a number between 1 to 100. Best Of Luck!`;
+    });
+
+}
+
